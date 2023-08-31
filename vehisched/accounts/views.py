c547c574c8498e21f42799312a07ce213ae5d066
rakss17 <bohariambulo17@gmail.com>
2023-08-31 20:22:00 +0800

    added send to the user email the temporary password

--- vehisched/accounts/views.py
+++ vehisched/accounts/views.py
@@ -6,6 +6,7 @@ from django.utils.http import urlsafe_base64_decode
 from django.contrib import messages
 from django.conf import settings
 from django.core.mail import send_mail
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
 
 User = get_user_model()
 
@@ -23,8 +24,13 @@ def activate_account(request, uidb64, token):
 
         activation_link = f"{settings.BASE_URL}/activation/{uidb64}/{token}/"
 
+        temporary_password = User.objects.make_random_password()
+        user.set_password(temporary_password)
+        user.save()
+
         subject = 'Your account has been activated'
-        message = f'Your account has been activated. You can now log in at {settings.BASE_URL}.'
+        message = f'Your account has been activated.\nUsername: {user.username}\nEmail: {user.email}\nPassword: {temporary_password}\nYou can now log in at {settings.BASE_URL}.'
+
         from_email = settings.EMAIL_HOST_USER
         to_email = user.email
 
