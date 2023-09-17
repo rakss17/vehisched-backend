92a8d2636b7fc9e61cb20441eaa721be9dd68e4a
rakss17 <bohariambulo17@gmail.com>
2023-09-17 19:01:50 +0800

    added vehicle post view API

--- vehisched/vehicle/urls.py
+++ vehisched/vehicle/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('fetch/', views.VehicleListCreateView.as_view(),
+    path('fetch-post/', views.VehicleListCreateView.as_view(),
          name='vehicle-list-create'),
     path('update/<int:pk>/', views.VehicleRetrieveUpdateDestroyView.as_view(),
          name='vehicle-retrieve-update-destroy'),
--- vehisched/vehicle/views.py
+++ vehisched/vehicle/views.py
@@ -1,11 +1,40 @@
 from rest_framework import generics
 from .models import Vehicle
 from .serializers import VehicleSerializer
+import requests
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.response import Response
+from rest_framework import status
 
 
 class VehicleListCreateView(generics.ListCreateAPIView):
+    # Requires token authentication
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
     queryset = Vehicle.objects.all()
     serializer_class = VehicleSerializer
+
+    def create(self, request, *args, **kwargs):
+        # Deserialize the request data using the serializer
+        serializer = self.get_serializer(data=request.data)
+
+        if serializer.is_valid():
+            # Save the vehicle object
+            self.perform_create(serializer)
+
+            # Handle image upload here
+            # Replace with your actual field name
+            image_file = request.FILES.get('vehicle_image')
+            if image_file:
+                # Do something with the image, e.g., save it to the vehicle object
+                serializer.instance.vehicle_image = image_file
+                serializer.instance.save()
+
+            headers = self.get_success_headers(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class VehicleRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
