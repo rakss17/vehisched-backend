68511c9b241e8eed2f556ed5608533967553f168
rakss17 <bohariambulo17@gmail.com>
2023-09-18 15:00:20 +0800

    removed some comments

--- vehisched/vehicle/views.py
+++ vehisched/vehicle/views.py
@@ -10,25 +10,23 @@ from rest_framework.parsers import MultiPartParser, FormParser
 
 
 class VehicleListCreateView(generics.ListCreateAPIView):
-    # Requires token authentication
+
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
     queryset = Vehicle.objects.all()
     serializer_class = VehicleSerializer
 
     def create(self, request, *args, **kwargs):
-        # Deserialize the request data using the serializer
+
         serializer = self.get_serializer(data=request.data)
 
         if serializer.is_valid():
-            # Save the vehicle object
+
             self.perform_create(serializer)
 
-            # Handle image upload here
-            # Replace with your actual field name
             image_file = request.FILES.get('vehicle_image')
             if image_file:
-                # Do something with the image, e.g., save it to the vehicle object
+
                 serializer.instance.vehicle_image = image_file
                 serializer.instance.save()
 
@@ -41,23 +39,22 @@ class VehicleListCreateView(generics.ListCreateAPIView):
 class VehicleRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Vehicle.objects.all()
     serializer_class = VehicleSerializer
-    parser_classes = (MultiPartParser, FormParser)  # Allow file uploads
+    parser_classes = (MultiPartParser, FormParser)
 
     def patch(self, request, *args, **kwargs):
-        instance = self.get_object()  # Get the instance to update
+        instance = self.get_object()
         serializer = self.get_serializer(
             instance, data=request.data, partial=True)
 
         if serializer.is_valid():
-            # Check if a new image was provided in the request
+
             new_image = request.FILES.get('vehicle_image')
             print("imnagege: ", new_image)
             if new_image:
-                # Delete the old image file (optional)
+
                 if instance.vehicle_image:
                     instance.vehicle_image.delete()
 
-                # Update the vehicle's image field with the new image
                 instance.vehicle_image = new_image
 
             serializer.save()
