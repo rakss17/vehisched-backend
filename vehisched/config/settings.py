d3cef442980244594e2081ddcb437f5e860a0d62
rakss17 <bohariambulo17@gmail.com>
2024-02-01 17:43:13 +0800

    changed celery redis port and added redis to docker-compose

--- vehisched/config/settings.py
+++ vehisched/config/settings.py
@@ -178,7 +178,7 @@ CHANNEL_LAYERS = {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
 
-            "hosts": [("localhost", 6379)],
+            "hosts": [("redis", 6381)],
         },
     },
 }
@@ -192,8 +192,8 @@ CHANNEL_LAYERS = {
 # }
 
 
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = 'redis://redis:6381/0'
+CELERY_RESULT_BACKEND = 'redis://redis:6381/0'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TASK_SERIALIZER = 'json'
